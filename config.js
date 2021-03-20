15d564ce51065d80cec2c8424eeae5f90541b077
Ruchir Rathore <rathoreruchir7@gmail.com>
2021-03-20 21:15:38 +0530

    removed mongoDB uri

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    // 'mongoUrl' : 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.9dt6n.mongodb.net/Oneshot?retryWrites=true&w=majority'
 }
